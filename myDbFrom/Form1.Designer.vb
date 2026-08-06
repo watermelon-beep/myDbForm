ed99c40e6a43994a78c15b5886657817520f5084
WATERMELON <WATERMELON@DESKTOP-4UPNDSR>
2026-08-06 19:07:24 +0800

    new ui and updated database

--- myDbFrom/Form1.Designer.vb
+++ myDbFrom/Form1.Designer.vb
@@ -23,10 +23,21 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.idtxbx = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.nametxbx = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.agetxbx = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.gradetxbx = New System.Windows.Forms.TextBox()
+        Me.coursecmbx = New System.Windows.Forms.ComboBox()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,12 +46,95 @@ Partial Class Form1
         Me.DataGridView1.AllowUserToAddRows = False
         Me.DataGridView1.AllowUserToDeleteRows = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
         Me.DataGridView1.Location = New System.Drawing.Point(33, 40)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(442, 354)
+        Me.DataGridView1.Size = New System.Drawing.Size(541, 354)
         Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(704, 358)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 36)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'idtxbx
+        '
+        Me.idtxbx.Location = New System.Drawing.Point(650, 57)
+        Me.idtxbx.Name = "idtxbx"
+        Me.idtxbx.Size = New System.Drawing.Size(180, 20)
+        Me.idtxbx.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(647, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(647, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Name"
+        '
+        'nametxbx
+        '
+        Me.nametxbx.Location = New System.Drawing.Point(650, 121)
+        Me.nametxbx.Name = "nametxbx"
+        Me.nametxbx.Size = New System.Drawing.Size(180, 20)
+        Me.nametxbx.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(647, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Age"
+        '
+        'agetxbx
+        '
+        Me.agetxbx.Location = New System.Drawing.Point(650, 190)
+        Me.agetxbx.Name = "agetxbx"
+        Me.agetxbx.Size = New System.Drawing.Size(180, 20)
+        Me.agetxbx.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(647, 243)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Grade"
+        '
+        'gradetxbx
+        '
+        Me.gradetxbx.Location = New System.Drawing.Point(650, 260)
+        Me.gradetxbx.Name = "gradetxbx"
+        Me.gradetxbx.Size = New System.Drawing.Size(180, 20)
+        Me.gradetxbx.TabIndex = 8
+        '
+        'coursecmbx
+        '
+        Me.coursecmbx.FormattingEnabled = True
+        Me.coursecmbx.Items.AddRange(New Object() {"BSIT", "HRM", "BMMA", "BSCS"})
+        Me.coursecmbx.Location = New System.Drawing.Point(650, 317)
+        Me.coursecmbx.Name = "coursecmbx"
+        Me.coursecmbx.Size = New System.Drawing.Size(180, 21)
+        Me.coursecmbx.TabIndex = 10
+        Me.coursecmbx.Text = "Course"
         '
         'Column1
         '
@@ -66,22 +160,50 @@ Partial Class Form1
         Me.Column4.Name = "Column4"
         Me.Column4.ReadOnly = True
         '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Course"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(948, 450)
+        Me.Controls.Add(Me.coursecmbx)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.gradetxbx)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.agetxbx)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.nametxbx)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.idtxbx)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents idtxbx As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents nametxbx As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents agetxbx As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents gradetxbx As TextBox
+    Friend WithEvents coursecmbx As ComboBox
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
 End Class
--- myDbFrom/Form1.vb
+++ myDbFrom/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Drawing.Drawing2D
 
 Public Class Form1
     Private Sub GroupBox1_Enter(sender As Object, e As EventArgs)
@@ -6,8 +7,35 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.BorderStyle = BorderStyle.Fixed3D
+        DataGridView1.BackgroundColor = Color.White
+        DataGridView1.RowHeadersVisible = False
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+
+        DataGridView1.EnableHeadersVisualStyles = False
+        DataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.SteelBlue
+        DataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black
+        DataGridView1.ColumnHeadersDefaultCellStyle.Font = New Font("Segoe UI", 10, FontStyle.Bold)
+
+        RoundDataGrid(DataGridView1, 20)
+
         connect()
         readData()
+    End Sub
+
+    Private Sub RoundDataGrid(dataGrid As DataGridView, radius As Integer)
+
+        Dim path As New GraphicsPath()
+
+        path.StartFigure()
+        path.AddArc(0, 0, radius, radius, 180, 90)
+        path.AddArc(dataGrid.Width - radius, 0, radius, radius, 270, 90)
+        path.AddArc(dataGrid.Width - radius, dataGrid.Height - radius, radius, radius, 0, 90)
+        path.AddArc(0, dataGrid.Height - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+
+        dataGrid.Region = New Region(path)
     End Sub
 
     Sub readData()
@@ -17,10 +45,31 @@ Public Class Form1
         sqldr = sqlcom.ExecuteReader
 
         While sqldr.Read
-            DataGridView1.Rows.Add(sqldr("student_id"), sqldr("student_name"), sqldr("student_age"), sqldr("student_grade"))
+            DataGridView1.Rows.Add(sqldr("student_id"), sqldr("student_name"), sqldr("student_age"), sqldr("student_grade"), sqldr("student_course"))
         End While
 
         sqlcom.Dispose()
         sqldr.Close()
+    End Sub
+
+    Sub saveData()
+        query = "Insert into student_record (student_id, student_name, student_age, student_grade, student_course) values (@student_id, @student_name, @student_age, @student_grade, @student_course)"
+        sqlcom = New SqlClient.SqlCommand(query, sqlconn)
+        With sqlcom.Parameters
+            .AddWithValue("@student_id", idtxbx.Text)
+            .AddWithValue("@student_name", nametxbx.Text)
+            .AddWithValue("@student_age", agetxbx.Text)
+            .AddWithValue("@student_grade", gradetxbx.Text)
+            .AddWithValue("@student_course", coursecmbx.Text)
+        End With
+
+        sqlcom.ExecuteNonQuery()
+        sqlcom.Dispose()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("added")
+        saveData()
+        readData()
     End Sub
 End Class
