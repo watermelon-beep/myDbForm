5b344de44f68101d18f308d93063fce9a6796f22
buffy duck <kianabante58@gmail.com>
2026-09-14 11:43:11 +0800

    Add image support to student records in WinForms app
    Added image column and update/delete buttons to DataGridView1. Integrated PictureBox and OpenFileDialog for image selection and display. Updated data loading and saving logic to handle student images as byte arrays. Modified form layout and .resx metadata for new controls. Added stub for image retrieval method.

--- myDbFrom/Form1.Designer.vb
+++ myDbFrom/Form1.Designer.vb
@@ -22,12 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle10 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle11 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle12 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column11 = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Column12 = New System.Windows.Forms.DataGridViewButtonColumn()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.idtxbx = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -44,20 +50,47 @@ Partial Class Form1
         Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.studentCounter = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.AllowUserToAddRows = False
         Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle10
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
-        Me.DataGridView1.Location = New System.Drawing.Point(60, 40)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column9, Me.Column11, Me.Column12})
+        DataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle11
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 34)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(541, 354)
+        DataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle12.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.RowHeadersDefaultCellStyle = DataGridViewCellStyle12
+        Me.DataGridView1.Size = New System.Drawing.Size(843, 240)
         Me.DataGridView1.TabIndex = 0
         '
         'Column1
@@ -90,9 +123,31 @@ Partial Class Form1
         Me.Column5.Name = "Column5"
         Me.Column5.ReadOnly = True
         '
+        'Column9
+        '
+        Me.Column9.HeaderText = "Image"
+        Me.Column9.Name = "Column9"
+        Me.Column9.ReadOnly = True
+        '
+        'Column11
+        '
+        Me.Column11.HeaderText = "Update"
+        Me.Column11.Name = "Column11"
+        Me.Column11.ReadOnly = True
+        Me.Column11.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column11.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'Column12
+        '
+        Me.Column12.HeaderText = "Delete"
+        Me.Column12.Name = "Column12"
+        Me.Column12.ReadOnly = True
+        Me.Column12.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column12.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(704, 358)
+        Me.Button1.Location = New System.Drawing.Point(383, 611)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(72, 36)
         Me.Button1.TabIndex = 1
@@ -101,7 +156,7 @@ Partial Class Form1
         '
         'idtxbx
         '
-        Me.idtxbx.Location = New System.Drawing.Point(650, 57)
+        Me.idtxbx.Location = New System.Drawing.Point(329, 310)
         Me.idtxbx.Name = "idtxbx"
         Me.idtxbx.Size = New System.Drawing.Size(180, 20)
         Me.idtxbx.TabIndex = 2
@@ -109,7 +164,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(647, 40)
+        Me.Label1.Location = New System.Drawing.Point(326, 293)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(18, 13)
         Me.Label1.TabIndex = 3
@@ -118,7 +173,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(647, 104)
+        Me.Label2.Location = New System.Drawing.Point(326, 357)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(35, 13)
         Me.Label2.TabIndex = 5
@@ -126,7 +181,7 @@ Partial Class Form1
         '
         'nametxbx
         '
-        Me.nametxbx.Location = New System.Drawing.Point(650, 121)
+        Me.nametxbx.Location = New System.Drawing.Point(329, 374)
         Me.nametxbx.Name = "nametxbx"
         Me.nametxbx.Size = New System.Drawing.Size(180, 20)
         Me.nametxbx.TabIndex = 4
@@ -134,7 +189,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(647, 173)
+        Me.Label3.Location = New System.Drawing.Point(326, 426)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(26, 13)
         Me.Label3.TabIndex = 7
@@ -142,7 +197,7 @@ Partial Class Form1
         '
         'agetxbx
         '
-        Me.agetxbx.Location = New System.Drawing.Point(650, 190)
+        Me.agetxbx.Location = New System.Drawing.Point(329, 443)
         Me.agetxbx.Name = "agetxbx"
         Me.agetxbx.Size = New System.Drawing.Size(180, 20)
         Me.agetxbx.TabIndex = 6
@@ -150,7 +205,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(647, 243)
+        Me.Label4.Location = New System.Drawing.Point(326, 496)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(36, 13)
         Me.Label4.TabIndex = 9
@@ -158,7 +213,7 @@ Partial Class Form1
         '
         'gradetxbx
         '
-        Me.gradetxbx.Location = New System.Drawing.Point(650, 260)
+        Me.gradetxbx.Location = New System.Drawing.Point(329, 513)
         Me.gradetxbx.Name = "gradetxbx"
         Me.gradetxbx.Size = New System.Drawing.Size(180, 20)
         Me.gradetxbx.TabIndex = 8
@@ -167,7 +222,7 @@ Partial Class Form1
         '
         Me.coursecmbx.FormattingEnabled = True
         Me.coursecmbx.Items.AddRange(New Object() {"BSIT", "HRM", "BMMA", "BSCS"})
-        Me.coursecmbx.Location = New System.Drawing.Point(650, 317)
+        Me.coursecmbx.Location = New System.Drawing.Point(329, 570)
         Me.coursecmbx.Name = "coursecmbx"
         Me.coursecmbx.Size = New System.Drawing.Size(180, 21)
         Me.coursecmbx.TabIndex = 10
@@ -211,17 +266,30 @@ Partial Class Form1
         '
         'studentCounter
         '
-        Me.studentCounter.Location = New System.Drawing.Point(905, 57)
+        Me.studentCounter.Location = New System.Drawing.Point(1076, 423)
         Me.studentCounter.Multiline = True
         Me.studentCounter.Name = "studentCounter"
         Me.studentCounter.Size = New System.Drawing.Size(100, 45)
         Me.studentCounter.TabIndex = 13
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(541, 310)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(119, 119)
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1904, 1041)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.studentCounter)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.DataGridView2)
@@ -240,6 +308,7 @@ Partial Class Form1
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -256,15 +325,20 @@ Partial Class Form1
     Friend WithEvents Label4 As Label
     Friend WithEvents gradetxbx As TextBox
     Friend WithEvents coursecmbx As ComboBox
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
-    Friend WithEvents Column5 As DataGridViewTextBoxColumn
     Friend WithEvents DataGridView2 As DataGridView
     Friend WithEvents Column6 As DataGridViewTextBoxColumn
     Friend WithEvents Column7 As DataGridViewTextBoxColumn
     Friend WithEvents Column8 As DataGridViewTextBoxColumn
     Friend WithEvents Button2 As Button
     Friend WithEvents studentCounter As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+    Friend WithEvents Column11 As DataGridViewButtonColumn
+    Friend WithEvents Column12 As DataGridViewButtonColumn
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- myDbFrom/Form1.vb
+++ myDbFrom/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.SqlClient
 Imports System.Drawing.Drawing2D
-
+Imports System.IO
+Imports System.Security.Cryptography
 Public Class Form1
 
     Private form2 As New Form2()
@@ -34,12 +35,28 @@ Public Class Form1
         sqlcom = New SqlClient.SqlCommand(query, sqlconn)
         sqldr = sqlcom.ExecuteReader
 
-        While sqldr.Read
-            DataGridView1.Rows.Add(sqldr("student_id"),
+        While sqldr.Read()
+
+            Dim img As Image = Nothing
+
+            If Not IsDBNull(sqldr("Image")) Then
+
+                Dim imageBytes() As Byte = CType(sqldr("Image"), Byte())
+
+                Using ms As New MemoryStream(imageBytes)
+                    img = Image.FromStream(ms)
+                End Using
+
+                DataGridView1.Rows.Add(sqldr("student_id"),
                                    sqldr("student_name"),
                                    sqldr("student_age"),
                                    sqldr("student_grade"),
-                                   sqldr("student_course"))
+                                   sqldr("student_course"),
+                                         img,
+                                         "Edit",
+                                         "Delete")
+
+            End If
         End While
 
         sqlcom.Dispose()
@@ -64,18 +81,24 @@ Public Class Form1
 
     Sub saveData()
 
+        Dim ms As New MemoryStream
+
+        PictureBox1.Image.Save(ms, PictureBox1.Image.RawFormat)
+
         query = "INSERT INTO student_record (
                 student_id,
                 student_name, 
                 student_age,
                 student_grade,
-                student_course
+                student_course,
+                image
                 )
                 VALUES (@student_id, 
                         @student_name, 
                         @student_age, 
                         @student_grade,
-                        @student_course)"
+                        @student_course,
+                        @image)"
 
         sqlcom = New SqlClient.SqlCommand(query, sqlconn)
 
@@ -85,10 +108,13 @@ Public Class Form1
             .AddWithValue("@student_age", agetxbx.Text)
             .AddWithValue("@student_grade", gradetxbx.Text)
             .AddWithValue("@student_course", coursecmbx.Text)
+            .AddWithValue("@image", ms.ToArray())
         End With
 
         sqlcom.ExecuteNonQuery()
         sqlcom.Dispose()
+
+        ms.Dispose()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -103,5 +129,30 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Hide()
         form2.Show()
+    End Sub
+
+    Sub viewImg()
+        Dim img() As Byte
+
+        query = "SELECT Image FROM student_record WHERE student_id = @student_id"
+        sqlcom = New SqlClient.SqlCommand(query, sqlconn)
+        sqldr = sqlcom.ExecuteReader
+
+        While sqldr.Read
+            If IsDBNull(sqldr("Image")) Then
+                PictureBox1.Image = Nothing
+            Else
+                img = sqldr("Image")
+            End If
+        End While
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        OpenFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+        End If
     End Sub
 End Class
